c81f24814e0e84910997a5f0b2ee5c04cbbba01b
Rafael Andrade <rafael@elosaude.com.br>
2023-01-05 15:25:32 -0300

    Correções do campo index.php

--- index.php
+++ index.php
@@ -67,30 +67,6 @@
 						<li><a href="#">Menu02</a></li>
 						<li><a href="#">Menu03</a></li>
 					 </ul>
-					</div>				
-					<div class="modulo vermelho-escuro">
-					 <h3>Workshops</h3>
-					 <ul>
-						<li><a href="#">Menu01</a></li>
-						<li><a href="#">Menu02</a></li>
-						<li><a href="#">Menu03</a></li>
-					 </ul>
-					</div>				
-					<div class="modulo azul-escuro">
-					 <h3>Treinamentos</h3>
-					 <ul>
-						<li><a href="#">Menu01</a></li>
-						<li><a href="#">Menu02</a></li>
-						<li><a href="#">Menu03</a></li>
-					 </ul>
-					</div>			
-		     		<div class="modulo roxo-escuro">
-					 <h3>Comunicados</h3>
-					 <ul>
-						<li><a href="#">Menu01</a></li>
-						<li><a href="#">Menu02</a></li>
-						<li><a href="#">Menu03</a></li>
-					 </ul>
 					</div>
 				</nav>
 			</div>
