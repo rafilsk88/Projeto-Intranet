d25519e353c300d5116a4e40380d0e5558f7ff29
Rafael Andrade <rafael@elosaude.com.br>
2023-01-06 13:41:02 -0300

    Campo de validação do formulario

--- index.php
+++ index.php
@@ -20,12 +20,12 @@
                     <form action="login.php" method="post" autocomplete="on">
 						<div class="campo">
 							<span class="material-icons">person</span>
-							<input type="email" name="login" id="ilogin" placeholder="seu e-mail" autocomplete="email">
+							<input type="email" name="login" id="ilogin" placeholder="seu e-mail" autocomplete="email" require maxlength="30">
 							<label for="ilogin">Login</label>
 						</div>
 						<div class="campo">
 							<span class="material-icons">vpn_key</span>
-							<input type="password" name="Senha" id="isenha" placeholder="sua senha" autocomplete="current-password">
+							<input type="password" name="Senha" id="isenha" placeholder="sua senha" autocomplete="current-password" required minlength="6" maxlength="8">
 							<label for="ilogin">Senha</label>
 						</div>
 						<input type="submit" value="Entrar">
