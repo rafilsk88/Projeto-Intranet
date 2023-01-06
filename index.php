dbe32a86409fd740edfd04248ada2ec7a32976df
Rafael Andrade <rafael@elosaude.com.br>
2023-01-06 02:02:09 -0300

    login

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 	<head>
 		<meta charset="UTF-8">		
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=MuseoModerno:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="recursos\css\style.css">
         <link rel="stylesheet" href="recursos\css\media-query.css">
 		<title>Elosaude</title>
 	</head>
-	<body>
-				
-		<main class="principal">
+	<body>				
+		<main>
 			<section id="login">
                 <div id="imagem">
 
@@ -22,8 +19,6 @@
                     <form action="login.php" method="post"></form>   
                 </div>
             </section>
-		</main>
-		
-	
+		</main>	
 	</body>
 </html>
