1d3dd5f2865209139a1969934e4c4cc44eca0cc4
Rafael Andrade <rafael@elosaude.com.br>
2023-02-06 22:43:49 -0300

    teste

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if(isset($_POST['email']) || isset($_POST['senha'])){
 		<meta charset="UTF-8">	
 		<meta http-equiv="X-UA-Compatible" content="IE-edge">	
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Elosaude</title>
+		<title>Login</title>
 	</head>
 	<body>				
 		<h1>Seja bem-vindo(a)!</h1>
