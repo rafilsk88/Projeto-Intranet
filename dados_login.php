915542265feb654edd5682de2e809ca075a59dbd
Rafael Andrade <rafael@elosaude.com.br>
2023-02-07 01:31:40 -0300

    dd

--- dados_login.php
+++ dados_login.php
@@ -2,32 +2,15 @@
 session_start();
 $_SESSION['logged'] = $_SESSION['logged'] ?? False;
 
-if(isset($_POST['email']) || isset($_POST['senha'])){
-
-if(strlen($_POST['email']) == 0) {
-    echo "Preencha seu e-mail";
-}else if(strlen($_POST['senha']) == 0){
-    echo "Preencha sua senha";
-} else {
-    $email = $mysqli->real_escape_string($_POST['email']);
-    $senha = $mysqli->real_escape_string($_POST['senha']);
-
-    $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
-    $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
-
-    $quantidade = $sql_query->num_rows;
-
-    if($quantidade == 1){
-
-        $usuario = $sql_query->fetch_assoc();
+$usuario_bd = rafael;
+$senha_bd = 1234;
         
-        $_SESSION['idusuario'] = $usuario['idusuario'];
-        $_SESSION['nome'] = $usuario['nome'];               
+$p_usuario = $_POST['email'];
+$p_senha = $_POST['senha'];
 
-
-    }else {
-        echo "Falha ao logar! E-mail ou senha incorretos";
-    }
+if($p_usuario == $usuario_bd && $p_senha == $senha_bd ){
+    $_SESSION['email'] = $usuario_bd;
+    $_SESSION['senha'] = $senha_bd;
+    $_SESSION['logged'] = True;      
 }
-}
-?>
+           
