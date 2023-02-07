3be0e455cd6a5fef3b8fa03943e4c8353e3cfeaa
Rafael Andrade <rafael@elosaude.com.br>
2023-02-06 23:45:24 -0300

    teste

--- comunicados/comunicados.php
+++ comunicados/comunicados.php
@@ -1,35 +1,21 @@
+<?php
+
+if(!isset($_SESSION)){
+	session_start();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8">		
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=MuseoModerno:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="..\recursos\css\estilo.css">
-		<link rel="stylesheet" href="..\recursos\css\acordo_coletivo.css">
-		<title>Elosaude</title>
+		<meta charset="UTF-8">	
+		<meta http-equiv="X-UA-Compatible" content="IE-edge">	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Home</title>
 	</head>
-	<body class="acordo_coletivo">
-		<header class="cabecalho">
-			<h1>Intranet</h1>
-			<h2>Elosaude</h2>
-		</header>
-		<nav class="navegacao">
-			<a href=<?="/{$_GET['dir']}/{$_GET['file']}.php"?> class="verde">Sem Formatação</a>
-			<a href="..\index.php" class="vermelho">Voltar</a>
-		</nav>
-		<main class="principal">
-			<div class="conteudo">
-				<?php
-					#include("/{$_GET['dir']}/{$_GET['file']}.php");
-					#include('elosaude/organograma.php')
-					include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
-				?>
-			</div>
-		</main>
-		<footer class="rodape">
-			ELOSAÚDE – Associação de Assistência à Saúde  © <?= date(Y) ?>
-		</footer>
-	
+	<body>				
+		<h1>Seja bem-vindo(a), <?php echo $_SESSION['nome'];?> </h1>
+		<h2> Ao nosso canal de comunicação da ELOSAUDE</h2>   
 	</body>
 </html>
