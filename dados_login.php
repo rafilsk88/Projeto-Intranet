7b836d4559b36958f6dc98c6c1aaf14a1bf8bd10
Rafael Andrade <rafael@elosaude.com.br>
2023-02-07 01:18:52 -0300

    ww

--- dados_login.php
+++ dados_login.php
@@ -1,8 +1,14 @@
 <?php 
-
 session_start();
 $_SESSION['logged'] = $_SESSION['logged'] ?? False;
 
+if(isset($_POST['email']) || isset($_POST['senha'])){
+
+if(strlen($_POST['email']) == 0) {
+    echo "Preencha seu e-mail";
+}else if(strlen($_POST['senha']) == 0){
+    echo "Preencha sua senha";
+} else {
     $email = $mysqli->real_escape_string($_POST['email']);
     $senha = $mysqli->real_escape_string($_POST['senha']);
 
@@ -14,12 +20,14 @@ $_SESSION['logged'] = $_SESSION['logged'] ?? False;
     if($quantidade == 1){
 
         $usuario = $sql_query->fetch_assoc();
-
+        
         $_SESSION['idusuario'] = $usuario['idusuario'];
-        $_SESSION['nome'] = $usuario['nome'];
+        $_SESSION['nome'] = $usuario['nome'];               
+
 
     }else {
         echo "Falha ao logar! E-mail ou senha incorretos";
     }
-
+}
+}
 ?>
