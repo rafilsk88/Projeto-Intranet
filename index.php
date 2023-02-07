ab7df91e7389ae11ffa42df02f622c80e5d6367e
Rafael Andrade <rafael@elosaude.com.br>
2023-02-06 23:52:28 -0300

    ll

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if(isset($_POST['email']) || isset($_POST['senha'])){
 			$_SESSION['idusuario'] = $usuario['idusuario'];
 			$_SESSION['nome'] = $usuario['nome'];
 
-			header("Location: comunicados/comunicados.php");
+			header('Location: http://192.168.40.25/Projeto-Intranet/home.php');
 			
 
 
