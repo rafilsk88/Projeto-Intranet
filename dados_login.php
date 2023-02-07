25760e79a33b12812dedce341f399d04d17fee0e
Rafael Andrade <rafael@elosaude.com.br>
2023-02-07 02:04:05 -0300

    sss

--- dados_login.php
+++ dados_login.php
@@ -1,5 +1,12 @@
 <?php 
 session_start();
+
+if(isset($_GET['logout']) && $_GET['logout'] == 1){
+    $_SESSION = array();
+    session_destroy();
+    header('location: /');
+}
+
 $_SESSION['logged'] = $_SESSION['logged'] ?? False;
 
 $usuario_bd = 'rafael';
