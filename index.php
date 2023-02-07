3dd6d4984d104ffbbd08f47189abb110bcc35fc4
Rafael Andrade <rafael@elosaude.com.br>
2023-02-06 21:39:54 -0300

    Tela de login

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
 <?php 
 include('conexao.php');
+
+if(isset($_POST['login']) || isset($_POST['Senha'])){
+
+	if(strlen($_POST['login']) == 0) {
+		echo "Preencha seu e-mail";
+	}else if(strlen($_POST['Senha']) == 0){
+		echo "Preencha sua senha";
+	} else {
+		$email = $mysqli->real_escape_string($_POST['login']);
+		$senha = $mysqli->real_escape_string($_POST['Senha']);
+
+		$sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+		$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+
+		$quantidade = $sql_query->num_rows;
+
+		if($quantidade == 1){
+			$usuario = $sql_query->fetch_assoc();
+
+			if(!isset($_SESSION)){
+				session_start();
+			}
+
+			$_SESSION['idusuario'] = $usuario['idusuario'];
+			$_SESSION['nome'] = $usuario['nome'];
+
+			header("Location: home.php");
+
+
+		}else {
+			echo "Falha ao logar! E-mail ou senha incorretos";
+		}
+	}
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
