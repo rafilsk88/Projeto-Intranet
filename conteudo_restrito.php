798dc1061453c5091d95f44a12015fe1458f5fac
Rafael Andrade <rafael@elosaude.com.br>
2023-02-07 00:57:23 -0300

    pp

--- conteudo_restrito.php
+++ conteudo_restrito.php
@@ -3,3 +3,10 @@ $logged = $_SESSION['logged'] ?? NULL;
 
 if(!$logged) die('Conteudo restrito, apenas para colaboradores da ELOSAUDE');
 ?>
+
+<p>
+    <?php
+    echo "Olá {$_SESSION['nome']}";
+    ?>
+
+</p>
