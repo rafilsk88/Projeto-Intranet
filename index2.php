6dc39e49cef8c37a059ecdb9206d40c90f02f9b7
Rafael Andrade <rafael@elosaude.com.br>
2023-02-06 22:10:36 -0300

    Alteração da pagina

--- index2.php
+++ index2.php
@@ -1,75 +1,64 @@
+<?php 
+include('conexao.php');
+
+if(isset($_POST['login']) || isset($_POST['Senha'])){
+
+	if(strlen($_POST['login']) == 0) {
+		echo "Preencha seu e-mail";
+	}else if(strlen($_POST['Senha']) == 0){
+		echo "Preencha sua senha";
+	} else {
+		$email = $mysqli->real_escape_string($_POST['login']);
+		$senha = $mysqli->real_escape_string($_POST['Senha']);
+
+		$sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+		$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+
+		$quantidade = $sql_query->num_rows;
+
+		if($quantidade == 1){
+			$usuario = $sql_query->fetch_assoc();
+
+			if(!isset($_SESSION)){
+				session_start();
+			}
+
+			$_SESSION['idusuario'] = $usuario['idusuario'];
+			$_SESSION['nome'] = $usuario['nome'];
+
+			header("Location: home.php");
+
+
+		}else {
+			echo "Falha ao logar! E-mail ou senha incorretos";
+		}
+	}
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8">		
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=MuseoModerno:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="recursos\css\estilo.css">
+		<meta charset="UTF-8">	
+		<meta http-equiv="X-UA-Compatible" content="IE-edge">	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Elosaude</title>
 	</head>
-	<body>
-		<header class="cabecalho">
-			<h1>Intranet</h1>
-			<h2>Fique por dentro de tudo que acontece em nossa associação</h2>
-		</header>		
-		<main class="principal">
-			<div class="conteudo">
-				<nav class="modulos">
-					<div class="modulo verde">
-					 <h3>Institucional</h3>
-					 <ul>
-						<li><a href="elosaude.php?dir=elosaude&file=estatuto_social">Estatuto Social</a></li>
-						<li><a href="elosaude.php?dir=elosaude&file=organograma">Organograma</a></li>
-						<li><a href="elosaude.php?dir=elosaude&file=acordo_coletivo">Acordo Coletivo</a></li>
-					 </ul>
-					</div>				
-					<div class="modulo vermelho">
-					 <h3>GNI</h3>
-					 <ul>
-						<li><a href="#">Menu01</a></li>
-						<li><a href="#">Menu02</a></li>
-						<li><a href="#">Menu03</a></li>
-					 </ul>
-					</div>				
-					<div class="modulo azul">
-					 <h3>Governança e Compliance</h3>
-					 <ul>
-						<li><a href="#">Menu01</a></li>
-						<li><a href="#">Menu02</a></li>
-						<li><a href="#">Menu03</a></li>
-					 </ul>
-					</div>			
-					<div class="modulo roxo">
-					 <h3>Gente e Gestão</h3>
-					 <ul>
-						<li><a href="#">Menu01</a></li>
-						<li><a href="#">Menu02</a></li>
-						<li><a href="#">Menu03</a></li>
-					 </ul>
-					</div>				
-					<div class="modulo laranja">
-					 <h3>Superintendente</h3>
-					 <ul>
-						<li><a href="#">Menu01</a></li>
-						<li><a href="#">Menu02</a></li>
-						<li><a href="#">Menu03</a></li>
-					 </ul>
-					</div>			
-					<div class="modulo verde-escuro">
-					 <h3>LGPD</h3>
-					 <ul>
-						<li><a href="#">Menu01</a></li>
-						<li><a href="#">Menu02</a></li>
-						<li><a href="#">Menu03</a></li>
-					 </ul>
-					</div>
-				</nav>
-			</div>
-		</main>
-		<footer class="rodape">
-			ELOSAÚDE – Associação de Assistência à Saúde  © <?= date(Y) ?>
-		</footer>
-	
+	<body>				
+		<h1>Seja bem-vindo(a) novamnete. Faça login para acessar a Intranet</h1>
+		<form action=" " method="post" autocomplete="on">
+			<p>
+				<label>E-mail</label>
+				<input type="text" name="email">
+			</p>
+			<p>
+				<label>Senha</label>
+				<input type="password" name="senha">
+			</p>
+			<p>
+				<button type="submit">Entrar<button>
+			</p>
+		</form>   
 	</body>
 </html>
