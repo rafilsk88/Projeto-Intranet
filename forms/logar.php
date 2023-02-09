df4012659c9509702f3d9a5e686383388ce9bbcc
Rafael Andrade <rafael@elosaude.com.br>
2023-02-09 01:33:45 -0300

    aa

--- forms/logar.php
+++ forms/logar.php
@@ -1,6 +1,6 @@
 <?php 
 require_once '../includes/session.php';
-require_once '../include/db.php';
+require_once '../includes/db.php';
 
 $p_usuario = $_POST['usuario'] ?? NULL;
 $p_senha = $_POST['senha'] ?? NULL;
@@ -9,7 +9,7 @@ if($p_usuario == $usuario_db && $p_senha == $senha_db){
     $_SESSION['usuario'] = $usuario_db;
     $_SESSION['senha'] = $usuario_db;
     $_SESSION['session_id'] = session_id();
-    header('Location: /');
+    header('Location: http://192.168.40.25/Projeto-Intranet/index.php');
     
 } else{
     session_unset();
