c9a910c1e74dfba57281e3e33bbb1e587da3a272
Rafael Andrade <rafael@elosaude.com.br>
2023-02-09 00:32:52 -0300

    teste

--- html/menu.php
+++ html/menu.php
@@ -1,5 +1,5 @@
 <p>
-    <a href="http://192.168.40.25/Projeto-Intranet/index.php">elosaude</a>
+    <a href="http://192.168.40.25/Projeto-Intranet/index.php">Home</a>
     <a href="http://192.168.40.25/Projeto-Intranet/paginas/pagina1.php">Pagina 1</a>
 	<a href="http://192.168.40.25/Projeto-Intranet/paginas/pagina2.php">Pagina 2</a>
 	<a href="http://192.168.40.25/Projeto-Intranet/paginas/pagina3.php">Pagina 3</a>
