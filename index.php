366e243457c8229a87b63519e1fbe4d0ed1c80b5
Rafael Andrade <rafael@elosaude.com.br>
2023-02-09 00:44:31 -0300

    tew

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include_once 'includes/session.php';
 		<meta charset="UTF-8">	
 		<meta http-equiv="X-UA-Compatible" content="IE-edge">	
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Login</title>
+		<title>Home</title>
 	</head>
 	<body>
 		<div class="container">
