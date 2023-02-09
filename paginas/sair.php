d7ccb087e64bcd1889123abf5cd71aee778df1c8
Rafael Andrade <rafael@elosaude.com.br>
2023-02-09 00:33:27 -0300

    teste

--- paginas/sair.php
+++ paginas/sair.php
@@ -2,4 +2,4 @@
 session_start();
 session_unset();
 session_destroy();
-header('Location: http://192.168.40.25/Projeto-Intranet/index.php')
+header('Location: http://192.168.40.25/Projeto-Intranet/index.php');
